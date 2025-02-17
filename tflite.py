a640dcf800cddd14fd42ceb8702b6608e632a199
seungboAn <seungbo1112@gmail.com>
2025-02-17 14:35:00 +0900

    Init files

--- tflite.py
+++ tflite.py
@@ -1,28 +1,41 @@
+from flask import Flask, request, jsonify
 import tensorflow as tf
-from tensorflow import keras
+import numpy as np
 
-load_path = 'mlops/best_model/1/model.keras'
-model = keras.models.load_model(load_path)
+app = Flask(__name__)
 
-# Keras 3 호환 형식으로 모델 재 저장
-model.save('mlops/best_model/1/model_keras_v3.keras') #다른이름으로 저장
-
-#재 저장된 모델 로드 확인
-model_v3 = keras.models.load_model('mlops/best_model/1/model_keras_v3.keras')
-model_v3.summary()
-
-converter = tf.lite.TFLiteConverter.from_keras_model(best_model)
-tflite_model = converter.convert()
-
-tflite_file_path = 'best_model.tflite'
-with open(tflite_file_path, 'wb') as f:
-    f.write(tflite_model)
-
-interpreter = tf.lite.Interpreter(model_path=tflite_file_path)
+# TensorFlow Lite 모델 로드
+interpreter = tf.lite.Interpreter(model_path="cifar10_tuned_model.tflite")
 interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-signatures = interpreter.get_signature_list()
-print(signatures)
+def preprocess_image(image_data):
+    # 이미지 전처리 (모델 입력 형태에 맞게 수정)
+    image = tf.image.decode_jpeg(image_data, channels=3) # 이미지 디코딩
+    image = tf.image.resize(image, (32, 32)) # 이미지 사이즈 조정
+    image = tf.cast(image, tf.float32) / 255.0 # 정규화
+    image = np.expand_dims(image, axis=0) # 배치 차원 추가
+    return image
 
-classify_lite = interpreter.get_signature_runner('serving_default')
-print(classify_lite)
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        image_file = request.files['image']
+        image_data = image_file.read()
+        image = preprocess_image(image_data)
+
+        # TensorFlow Lite 모델 추론
+        interpreter.set_tensor(input_details[0]['index'], image)
+        interpreter.invoke()
+        output = interpreter.get_tensor(output_details[0]['index'])
+
+        # 예측 결과 반환
+        prediction = np.argmax(output)
+        return jsonify({'prediction': int(prediction)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8501)
